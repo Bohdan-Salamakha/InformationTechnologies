79aed349e2e59d6ce520a838b8fdfae83729b06d
Bohdan Salamakha <allen.avanheim@gmail.com>
2024-05-09 03:49:00 +0200

    feat: 1.added tests for the Document viewset

--- lab1/tests.py
+++ lab1/tests.py
@@ -45,11 +45,16 @@ class DocumentViewSetTest(APITestCase):
 
     def test_update_document_permission(self):
         url = reverse('lab1:document-detail', kwargs={'pk': self.document.pk})
-        data = {'name': 'Updated Name'}
+        data = {
+            'name': 'Updated Name',
+            'doc_type': Document.DocumentType.EMPLOYMENT_APPLICATION,
+            'body': 'Updated body',
+        }
         response = self.client.put(url, data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.document.refresh_from_db()
         self.assertEqual(self.document.name, 'Updated Name')
+        self.assertEqual(self.document.body, 'Updated body')
 
     def test_delete_document_permission(self):
         url = reverse('lab1:document-detail', kwargs={'pk': self.document.pk})
@@ -57,6 +62,66 @@ class DocumentViewSetTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Document.objects.count(), 0)
 
-    def tearDown(self):
-        self.user.delete()
+    def test_filter_documents_signed(self):
         Document.objects.all().delete()
+        Document.objects.create(
+            name="Signed Document",
+            doc_type=Document.DocumentType.VACATION_REQUEST,
+            body="Signed Body",
+            user=self.user,
+            signature_date="2021-01-01"
+        )
+        Document.objects.create(
+            name="Unsigned Document",
+            doc_type=Document.DocumentType.VACATION_REQUEST,
+            body="Unsigned Body",
+            user=self.user
+        )
+
+        response = self.client.get(reverse('lab1:document-list'), {'is_signed': 'true'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], "Signed Document")
+
+        response = self.client.get(reverse('lab1:document-list'), {'is_signed': 'false'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], "Unsigned Document")
+
+    def test_filter_documents_by_date(self):
+        response = self.client.get(
+            reverse('lab1:document-list'),
+            {'start_date': '2020-01-01', 'end_date': '2030-12-31'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(response.data) > 0)
+
+    def test_update_document_permission_denied(self):
+        other_user = User.objects.create_user(username='other_user', password='67890')
+        self.client.force_authenticate(user=other_user)
+        url = reverse('lab1:document-detail', kwargs={'pk': self.document.pk})
+        data = {'name': 'Unauthorized Update'}
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_delete_document_permission_denied(self):
+        other_user = User.objects.create_user(username='other_user', password='67890')
+        self.client.force_authenticate(user=other_user)
+        url = reverse('lab1:document-detail', kwargs={'pk': self.document.pk})
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_document_invalid_data(self):
+        url = reverse('lab1:document-list')
+        data = {
+            'name': '',
+            'doc_type': Document.DocumentType.EMPLOYMENT_APPLICATION,
+            'body': 'Some Body'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def tearDown(self):
+        super().tearDown()
+        Document.objects.all().delete()
+        User.objects.all().delete()
